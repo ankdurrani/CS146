9f4e3cc05caebf7d8effba11497bd4e2c6d75e95
ankdurrani <143302842+ankdurrani@users.noreply.github.com>
2024-03-23 23:19:55 -0700

    Update HW9.py

--- HW/HW9/HW9.py
+++ HW/HW9/HW9.py
@@ -1,1 +1,15 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
+def lowestCommonAncestor(root, p, q):
+    if root is None:
+        return None
+    if root.val > p.val and root.val > q.val:
+        return lowestCommonAncestor(root.left, p, q)
+    elif root.val < p.val and root.val < q.val:
+        return lowestCommonAncestor(root.right, p, q)
+    else:
+        return root
