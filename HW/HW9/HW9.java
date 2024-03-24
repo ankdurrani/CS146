496af3e8ebbe8db8540020859512924394484918
ankdurrani <143302842+ankdurrani@users.noreply.github.com>
2024-03-23 23:18:48 -0700

    Update HW9.java

--- HW/HW9/HW9.java
+++ HW/HW9/HW9.java
@@ -1,1 +1,39 @@
+ public class TreeNode {
+
+     int val;
+
+     TreeNode left;
+
+     TreeNode right;
+
+     TreeNode() {}
+
+     TreeNode(int val) { this.val = val; }
+
+     TreeNode(int val, TreeNode left, TreeNode right) {
+
+         this.val = val;
+
+         this.left = left;
+
+         this.right = right;
+
+     }
+
+  }
+
+public class HW9 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
+}
 
