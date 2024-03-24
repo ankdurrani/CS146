0510e157b45086da64339aa51d5b4c6ef0b916fe
ankdurrani <143302842+ankdurrani@users.noreply.github.com>
2024-03-23 23:25:35 -0700

    Update HW11.java

--- HW/HW11/HW11.java
+++ HW/HW11/HW11.java
@@ -1,1 +1,21 @@
+public class HW11 {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        if (image[sr][sc] == newColor) {
+            return image;
+        }
+        int oldColor = image[sr][sc];
+        dfs(image, sr, sc, oldColor, newColor);
+        return image;
+    }
+    private void dfs(int[][] image, int r, int c, int oldColor, int newColor) {
+        if (r < 0 || r >= image.length || c < 0 || c >= image[0].length || image[r][c] != oldColor) {
+            return;
+        }
+        image[r][c] = newColor;
+        dfs(image, r + 1, c, oldColor, newColor);
+        dfs(image, r - 1, c, oldColor, newColor);
+        dfs(image, r, c + 1, oldColor, newColor);
+        dfs(image, r, c - 1, oldColor, newColor);
+    }
+}
 
