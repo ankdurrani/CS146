191a0df49bd3a80cee966dbb4cf2329fb14b9eb6
ankdurrani <143302842+ankdurrani@users.noreply.github.com>
2024-04-21 23:51:11 -0700

    Update Lab6.java

--- Labs/Lab6/Lab6.java
+++ Labs/Lab6/Lab6.java
@@ -37,7 +37,7 @@ public class lab6 {
     public static void main(String[] args) {
         lab6 cs = new lab6();
         int numCourses = 2;
-        int[][] prerequisites = {{1, 0}, {0,1};
+        int[][] prerequisites = {{1, 0}, {0,1}};
         boolean canFinish = cs.canFinish(numCourses, prerequisites);
         System.out.println(canFinish);
     }
