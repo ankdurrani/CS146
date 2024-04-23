44c59c95faed15e8d0d2f051f4bde9a1b77f0515
ankdurrani <143302842+ankdurrani@users.noreply.github.com>
2024-04-23 00:26:16 -0700

    Update HW12.java

--- HW/HW12/HW12.java
+++ HW/HW12/HW12.java
@@ -1,1 +1,74 @@
+import java.util.*;
+
+class Edge implements Comparable<Edge> {
+    int from;
+    int to;
+    int cost;
+
+    public Edge(int from, int to, int cost) {
+        this.from = from;
+        this.to = to;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Edge other) {
+        return this.cost - other.cost;
+    }
+}
+
+public class WaterSupplyCost {
+    public int minCostToSupplyWater(int n, int[] wells, int[][] pipes) {
+        List<Edge> edges = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            edges.add(new Edge(0, i + 1, wells[i]));
+        }
+
+        for (int[] pipe : pipes) {
+            edges.add(new Edge(pipe[0], pipe[1], pipe[2]));
+        }
+
+        Collections.sort(edges);
+
+        int totalCost = 0;
+        int[] parent = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        for (Edge edge : edges) {
+            int rootFrom = find(parent, edge.from);
+            int rootTo = find(parent, edge.to);
+
+            if (rootFrom != rootTo) {
+                totalCost += edge.cost;
+                union(parent, rootFrom, rootTo);
+            }
+        }
+
+        return totalCost;
+    }
+
+    private int find(int[] parent, int node) {
+        if (parent[node] != node) {
+            parent[node] = find(parent, parent[node]);
+        }
+        return parent[node];
+    }
+
+    private void union(int[] parent, int node1, int node2) {
+        int root1 = find(parent, node1);
+        int root2 = find(parent, node2);
+        parent[root1] = root2;
+    }
+
+    public static void main(String[] args) {
+        WaterSupplyCost solution = new WaterSupplyCost();
+        int n = 2;
+        int[] wells = {1, 1};
+        int[][] pipes = {{1, 2, 1}, {1, 2, 2}};
+        System.out.println(solution.minCostToSupplyWater(n, wells, pipes));
+    }
+}
 
